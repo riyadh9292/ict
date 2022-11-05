377529b84ce1a6ad948c828fcdc8dfa392377172
T. H. Riyadh <mrriyadh66@gmail.com>
2022-11-05 22:39:23 +0600

    content added in graduate and undergraduate programme

--- resources/views/frontend/program/undergraduate.blade.php
+++ resources/views/frontend/program/undergraduate.blade.php
@@ -1,97 +1,113 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.frontend.app')
 
-<head>
+@section('styles')
+<style>
+    /* .show{
+        color: white;
+        background-color: yellow;
+    } */
+    /* .notices-banner {
+        background-image: url("{{ asset('assets/images/banner/banner3.jpg') }}")
+    } */
+</style>
+@endsection
 
-    <!-- META ============================================= -->
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="keywords" content="" />
-    <meta name="author" content="" />
-    <meta name="robots" content="" />
-
-    <!-- DESCRIPTION -->
-    <meta name="description" content="Department of ICT, MBSTU" />
-
-    <!-- OG -->
-    <meta property="og:title" content="Department of ICT, MBSTU" />
-    <meta property="og:description" content="Department of ICT, MBSTU" />
-    <meta property="og:image" content="" />
-    <meta name="format-detection" content="telephone=no">
-
-    <!-- FAVICONS ICON ============================================= -->
-    <link rel="icon" href="{{asset('assets/images/favicon.ico')}}" type="image/x-icon" />
-    <link rel="shortcut icon" type="image/x-icon" href="{{asset('assets/images/favicon.png')}}" />
-
-    <!-- PAGE TITLE HERE ============================================= -->
-    <title>Department of ICT, MBSTU</title>
-
-    <!-- MOBILE SPECIFIC ============================================= -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!--[if lt IE 9]>
-	<script src="assets/js/html5shiv.min.js"></script>
-	<script src="assets/js/respond.min.js"></script>
-	<![endif]-->
-
-    <!-- All PLUGINS CSS ============================================= -->
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/assets.css')}}">
-
-    <!-- TYPOGRAPHY ============================================= -->
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/typography.css')}}">
-
-    <!-- SHORTCODES ============================================= -->
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/shortcodes/shortcodes.css')}}">
-
-    <!-- STYLESHEETS ============================================= -->
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/style.css')}}">
-    <link class="skin" rel="stylesheet" type="text/css" href="{{asset('assets/css/color/color-1.css')}}">
-
-    <!-- REVOLUTION SLIDER CSS ============================================= -->
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/vendors/revolution/css/layers.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/vendors/revolution/css/settings.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/vendors/revolution/css/navigation.css')}}">
-    <!-- REVOLUTION SLIDER END -->
-</head>
-
-<body id="bg">
-    <div class="page-wraper">
-        <div id="loading-icon-bx"></div>
-        <div class="account-form">
-            <div class="account-head">
-                <a href="{{ route('welcome') }}"><img src="{{asset('assets/images/MBSTU_Logo.png')}}" alt=""></a>
-            </div>
-            <div class="account-form-inner">
-                <div class="account-container">
-                    <div class="error-page">
-                        <!-- <h3>Ooopps :(</h3>
-                        <h2 class="error-title">404</h2> -->
-                        <h5>This page is under construction</h5>
-                        <!-- <p>The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.</p> -->
-                        <div class="">
-                            <a href="{{ route('welcome') }}" class="btn outline black">Back To Home</a>
-                        </div>
-                    </div>
-                </div>
+@section('content')
+<div class="page-content bg-white">
+    <!-- inner page banner -->
+    <div class="page-banner ovbl-dark notices-banner">
+        <div class="container">
+            <div class="page-banner-entry">
+                <h1 class="text-white">Undergraduate</h1>
             </div>
         </div>
     </div>
-    <!-- External JavaScripts -->
-    <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
-    <!-- <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script> -->
-    <script src="{{ asset('assets/vendors/bootstrap/js/popper.min.js') }}"></script>
-    <script src="{{ asset('assets/vendors/bootstrap/js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('assets/vendors/bootstrap-select/bootstrap-select.min.js') }}"></script>
-    <script src="{{ asset('assets/vendors/bootstrap-touchspin/jquery.bootstrap-touchspin.js') }}"></script>
-    <script src="{{ asset('assets/vendors/magnific-popup/magnific-popup.js') }}"></script>
-    <script src="{{ asset('assets/vendors/counter/waypoints-min.js') }}"></script>
-    <script src="{{ asset('assets/vendors/counter/counterup.min.js') }}"></script>
-    <script src="{{ asset('assets/vendors/imagesloaded/imagesloaded.js') }}"></script>
-    <script src="{{ asset('assets/vendors/masonry/masonry.js') }}"></script>
-    <script src="{{ asset('assets/vendors/masonry/filter.js') }}"></script>
-    <script src="{{ asset('assets/vendors/owl-carousel/owl.carousel.js') }}"></script>
-    <script src="{{ asset('assets/js/functions.js') }}"></script>
-    <script src="{{ asset('assets/js/contact.js')}}"></script>
-</body>
+    <div role="tabpanel">
 
-</html>
+    <div class="p-[30px]">
+        <!-- card heading -->
+        <div class="mb-[30px]">
+        <p class="text-[30px] font-bold py-[30px]">Department of Information and Communication Technology</p>
+        <div class="shadow-lg rounded p-[30px] w-full 2xl:w-1/2">
+            <div class="flex items-center gap-[30px]">
+            <div class="text-xl">FACULTY</div>
+            <div class="w-[1px] h-5 bg-gray-600"></div>
+            <div class="text-xl">DURATION</div>
+            </div>
+            <div class="flex items-center gap-[30px] mt-4">
+            <div class="text-lg">Engineering</div>
+            <div class=""></div>
+            <div class="text-lg text-gray-600">4 years</div>
+            </div>
+           
+        </div>
+
+        </div>
+    <ul class="nav  nav-justified mt-[60px]" role="tablist">
+    <li role="presentation" class="active">
+  <a class="text-xl  border p-4" href="#home" aria-controls="home" role="tab" data-toggle="tab">
+    OVERVIEW
+</a>
+</li>
+    <li role="presentation">
+  <a class="text-xl  border p-4" href="#profile" aria-controls="profile" role="tab" data-toggle="tab">
+    CURRICULAM & COURSES
+</a>
+</li>
+    <li role="presentation">
+  <a class="text-xl  border p-4" href="#settings" aria-controls="settings" role="tab" data-toggle="tab">
+      ALL SYLLABUS
+  </a>
+</li>
+       
+</ul>
+<div class="tab-content">
+    <div role="tabpanel" class="tab-pane active" id="home">
+<div class="col-md-9 col-xs-12"> 
+    <div class="p-[30px]">
+    <p class="pb-2.5 text-lg font-semibold"> Program Description </p>
+    <p class="text-base">Data not available</p>
+    </div>
+
+</div>
+
+</div>
+    <div role="tabpanel" class="tab-pane" id="profile">
+<div class="col-md-9 col-xs-12">
+<div class="p-[30px]">
+    <p class="pb-2.5 text-lg font-semibold"> Program Description </p>
+    <p class="text-base">Course data not available</p>
+    </div>
+</div>
+      
+</div>
+    <div role="tabpanel" class="tab-pane" id="settings">
+<div class="col-md-9 col-xs-12">
+<div class="p-[30px]">
+    <p class="pb-2.5 text-lg font-semibold"> Program Description </p>
+    <p class="text-base">Program description not available</p>
+    </div>
+</div> 
+    </div>
+
+   
+
+<!-- Nav tabs -->
+
+
+<!-- Tab panes -->
+      
+</div>      
+</div>
+
+</div>
+
+    <!-- Breadcrumb row -->
+   
+    <!-- Breadcrumb row END -->
+    <!-- inner page banner END -->
+   
+@endsection
+
+@section('scripts')
+@endsection
